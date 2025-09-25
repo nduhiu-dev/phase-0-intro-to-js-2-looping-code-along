4bb6025146ed63e38e0a3fde1f64846d3c5565e0
Emmanuel Nduhiu <nduhiuemmanuel@gmail.com>
2025-09-25 20:17:04 +0300

    Completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// 1. WriteCards function
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// 2. CountDown function
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
